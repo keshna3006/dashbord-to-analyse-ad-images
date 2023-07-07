61deb086cbc9cbfe58b629f1d1a7ddb251c170df
Keshna <keshnatrivedi@gmail.com>
2023-07-07 11:32:49 +0530

    third commit

--- dashboard_for_ad_images/app_imgs/views.py
+++ dashboard_for_ad_images/app_imgs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from dashboard_for_ad_images.util import impressions_df,clicks_df
+from dashboard_for_ad_images.util import impressions_df,clicks_df,returndf,convert_rgb_to_names
 # Create your views here.
 def dashboard(request):
     impressions_data=impressions_df['impressions'].tolist()
@@ -16,11 +16,21 @@ def upload_view(request):
     if request.method == 'POST':
         # Logic to process the uploaded file
         file = request.FILES.get('csv_file')
+        dataframe=returndf(file)
+        impressions_data=dataframe['impressions'].tolist()
+        clicks_data=dataframe['clicks'].tolist()
+        labels=dataframe['colors'].tolist()
+
         # Perform any required operations with the file
         # For example, save the file to a specific location or process its content
 
         # Add a variable to the context to indicate that the file has been uploaded
-        context = {'file_uploaded': True}
-        return render(request, 'upload.html', context)
+        context = {
+            'imp_data':impressions_data,
+        'labels':labels,
+        'clicks_data':clicks_data,
+        
+        }
+        return render(request, 'dashboardagain.html', context)
 
     return render(request, 'upload.html')
--- dashboard_for_ad_images/dashboard_for_ad_images/util.py
+++ dashboard_for_ad_images/dashboard_for_ad_images/util.py
@@ -63,3 +63,39 @@ impressions_df['impressions']=impressions_column
 clicks_df=pd.DataFrame(colors_in_img)
 clicks_df.columns=['colors']
 clicks_df['clicks']=clicks_column
+
+def returndf(csvfile):
+    csv_file_path='sample.csv'
+    dataframe=pd.read_csv(csv_file_path)
+    img_column=dataframe['creative_img']
+    impressions_column=dataframe['impressions']
+    clicks_column=dataframe['clicks']
+    colors_in_img=[]
+
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS']='using-vision-api-for-dashboard-60c68f966514.json'
+    client=vision.ImageAnnotatorClient()
+    image=types.Image()
+
+    for i in img_column:
+        image.source.image_uri=i
+        response_image=client.image_properties(image=image)
+
+        image_data = []
+
+        for c in response_image.image_properties_annotation.dominant_colors.colors[:1]:
+           d = {
+               'color': c.color,
+               'score': c.score,
+               'pixel_fraction': c.pixel_fraction
+            }
+           red=c.color.red
+           green=c.color.green
+           blue=c.color.blue
+           colors_in_img.append(convert_rgb_to_names(red,green,blue))
+
+    dftoreturn=pd.DataFrame(colors_in_img)
+    dftoreturn.columns=['colors']
+    dftoreturn['impressions']=impressions_column
+    dftoreturn['clicks']=clicks_column
+
+    return dftoreturn
